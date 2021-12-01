d08c3d12488172edbb85e51411942c769eb85e27
GitHub Action <action@github.com>
2021-12-01 02:48:13 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/zhonghangit/androidstudiotemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.zhonghangit.androidstudiotemplate.services
+
+import com.github.zhonghangit.androidstudiotemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
